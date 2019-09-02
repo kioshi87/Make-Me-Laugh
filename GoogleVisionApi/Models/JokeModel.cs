ffcade1d7d0e0b2af767d241d21dcb01350d6ea8
Seth Blakely <seth.blakely@gmail.com>
2019-09-02 09:54:48 -0400

    Added joke and player models -- some questions included in player properties and added to MindMeister documentation

---
+++ GoogleVisionApi/Models/JokeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoogleVisionApi.Models
+{
+    public class JokeModel
+    {
+        public string Category { get; set; }
+        public bool Type { get; set; }
+        public string Joke { get; set; }
+        public string Setup { get; set; }
+        public string Deliver { get; set; }
+        public string Nsfw { get; set; }
+        public string Religious { get; set; }
+        public string Political { get; set; }
+        public string Id { get; set; }
+    }
+}
---
+++ GoogleVisionApi/Models/PlayerModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoogleVisionApi.Models
+{
+    public class PlayerModel
+    {
+        public string Player { get; set; }
+        public string Score { get; set; }
+        public string PreferredJokeCategory { get; set; } // Set to joke API categories:  Programming, Miscellaneous, Dark and Any
+        public string FunniestJoke { get; set; } // refers to Joke Id from joke API
+        public string FunniestPictures { get; set; } // Create a picture class and DB or save pictures directly to player DB and set ID string?
+        public string ContentFlagSettings { get; set; } // Should each flag be a public bool?  Nsfw = true, Political = false, etc.
+    }
+}
