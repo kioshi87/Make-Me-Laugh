d5bc9e2df07078ee34e2ee317ac90407da05fcff
Matthew Yo <matthewyo@gmail.com>
2019-09-10 15:47:05 -0400

    clean update

--- GoogleVisionApi/Program.cs
+++ GoogleVisionApi/Program.cs
@@ -9,10 +9,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
+
 namespace GoogleVisionApi
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
